0213f571fc6c51937b7b64324cac49e0a94638d1
Mohamed Nashaat <mohamed.nashaat@mondia.com>
2021-04-21 04:42:25 +0200

    adding feature to ommry

--- src/components/CalculateAge.vue
+++ src/components/CalculateAge.vue
@@ -24,13 +24,62 @@
         <v-btn @click="calcAge">إحسب عمرك</v-btn>
       </v-col>
     </v-row>
-    <v-row>
-      <v-col>{{ timeDiff }} </v-col>
+
+    <v-row v-if="date1">
+      <v-col cols="12" md="6">
+        عمرك بالميلادي :
+        {{ date1 }}
+      </v-col>
+      <v-col cols="12" md="6">
+        عمرك بالهجري :
+        {{ date2 }}
+      </v-col>
     </v-row>
+    <v-row>
+      <v-col cols="12">
+        عمرك بالشهور : {{ Math.floor(timeDiff / 1000 / 3600 / 24  / 30) }}
+      </v-col>
+      <v-col cols="12">
+        عمرك بالآسابيع : {{ Math.floor(timeDiff / 1000 / 3600 / 24 / 7) }}
+      </v-col>
+      <v-col cols="12">
+        عمرك بالأيام : {{ Math.floor(timeDiff / 1000 / 3600 / 24) }}
+      </v-col>
+      <v-col cols="12">
+        عمرك بالساعات : {{ Math.floor(timeDiff / 1000 / 60 / 60) }}
+      </v-col>
+      <v-col cols="12">
+        عمرك بالدقائق : {{ Math.floor(timeDiff / 1000 / 60) }}
+      </v-col>
+      <v-col cols="12">
+        عمرك الثواني : {{ Math.floor(timeDiff / 1000) }}
+      </v-col>
+    </v-row>
+
+    <!-- <v-row justify="center">
+      <v-col cols="12" md="6">
+        <v-data-table
+          :headers="headers"
+          :items="TableData1"
+          hide-default-footer
+          hide-default-header
+          class="elevation-1"
+        ></v-data-table>
+      </v-col>
+      <v-col cols="12" md="6">
+        <v-data-table
+          :headers="headers"
+          :items="TableData2"
+          hide-default-footer
+          hide-default-header
+          class="elevation-1"
+        ></v-data-table>
+      </v-col>
+    </v-row> -->
   </v-container>
 </template>
 <script>
-import { main } from "./script";
+import { main, ConvertAgeDiffToString } from "./script";
 
 export default {
   //   mounted() {
@@ -38,21 +87,50 @@ export default {
   //   },
   methods: {
     calcAge() {
-      main(this.date, this.dateType);
-    },
-  },
-  computed: {
-    timeDiff() {
-      if (this.date) {
-        let { _data } = main(this.date, this.dateType);
-
-        return _data;
+      this.TableData1 = [];
+      this.TableData2 = [];
+      let { gregAge, higiriAge, timeDiff } = main(this.date, this.dateType);
+      this.date1 = ConvertAgeDiffToString(gregAge._data);
+      this.date2 = ConvertAgeDiffToString(higiriAge._data);
+      this.timeDiff = timeDiff;
+      for (let i in gregAge._data) {
+        this.TableData1.push({ text: i, value: gregAge._data[i] });
       }
-      return "";
+      if (this.update === 0) {
+        this.update++;
+        setInterval(() => {
+          this.calcAge();
+        }, 1000);
+      }
     },
   },
+  // computed: {
+  //   timeDiff() {
+  //     if (this.date) {
+  //       console.log(main(this.date, this.dateType));
+  //       let { _data } = main(this.date, this.dateType);
+
+  //       return _data;
+  //     }
+  //     return "";
+  //   },
+  // },
   data() {
     return {
+      timeDiff: 0,
+      update: 0,
+      date1: null,
+      date2: null,
+      headers: [
+        {
+          value: "text",
+        },
+        {
+          value: "value",
+        },
+      ],
+      TableData1: [],
+      TableData2: [],
       dateType: "hijri",
       date: "1441-09-06",
       dateTypeMenu: [
--- src/components/script.js
+++ src/components/script.js
@@ -1,14 +1,67 @@
 var moment = require('moment-hijri');
+var _ = require('lodash');
 moment().format('iYYYY/iM/iD');
 
 
+let words = {
+    "days": "يوم",
+    "years": "سنة",
+    "months": "شهر",
+    "minutes": "دقيقة",
+    "hours": "ساعة",
+    "seconds": "ثانية"
+}
+
+
 function main(date, dateType) {
-    console.log(date, dateType)
+    // console.log(date, dateType)
     let x, y = moment()
     if (dateType === 'hijri') x = moment(date, "iYYYY-iM-iD")
     else x = moment(date, "YYYY-MM-DD")
-    return moment.duration(y.diff(x))
+
+
+    let diff = moment.duration(y.diff(x))
+
+    let higiriAge = _.cloneDeep(diff)
+
+    let YearDiff = y.iYear() - x.iYear(), MonthDiff = y.iMonth() - x.iMonth(), DayDiff = y.iDate() - x.iDate()
+    YearDiff = YearDiff - (MonthDiff > 0 ? 0 : 1)
+    MonthDiff = YearDiff >= 0 ? (MonthDiff + 12) % 12 : MonthDiff
+
+
+
+    higiriAge._data.years = YearDiff
+    higiriAge._data.months = MonthDiff
+    higiriAge._data.days = DayDiff
+
+
+
+
+    return { gregAge: diff, higiriAge, timeDiff: y.diff(x) }
 }
+
+function ConvertAgeDiffToString(age) {
+    let x = ""
+    let keys = Object.keys(age).reverse()
+
+
+    for (let i of keys) {
+
+
+        if (words[i] && age[i]) {
+            x += age[i] + " " + words[i] + " و "
+        }
+    }
+
+
+
+
+    x = x.slice(0, x.length - 3)
+    return x
+
+}
+
+
 
 // let m = moment('1410/8/28', 'iYYYY/iM/iD'); // Parse a Hijri date.
 // m.format('iYYYY/iM/iD [is] YYYY/M/D'); // 1410/8/28 is 1990/3/25
@@ -42,4 +95,4 @@ function main(date, dateType) {
 // // Complex parse:
 // moment('1990 5 25', 'YYYY iM D').format('YYYY/MM/DD'); // 1990/03/25
 
-export { main }
+export { main, ConvertAgeDiffToString }
