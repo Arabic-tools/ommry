f35bb59de4a610e8038e78a672e5541ce2635ae3
romell491 <ulgasan491@yahoo.com>
2021-10-07 14:28:41 +0800

    Update main.js
    Removed vue-gtag.

--- src/main.js
+++ src/main.js
@@ -2,11 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import VueSocialSharing from 'vue-social-sharing'
-import VueGtag from "vue-gtag";
-
-Vue.use(VueGtag, {
-  config: { id: "UA-208155997-5" }
-});
 
 Vue.use(VueSocialSharing);
 Vue.config.productionTip = false
